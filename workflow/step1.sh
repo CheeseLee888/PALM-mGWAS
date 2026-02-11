181f1831491c738bc4d9fb7e404574997cbe6416
CheeseLee888 <cheese.lee.888@gmail.com>
2026-02-10 23:22:20 -0600

    Recover some useful pkg in pixi; Delete PALMmethod and its logic

--- workflow/step1.sh
+++ workflow/step1.sh
@@ -4,17 +4,6 @@ source "$WORK/config.sh"
 
 
 ################################# workflow below (do not modify) #################################
-if [[ "${PALMmethod}" != 1 && "${PALMmethod}" != 2 ]]; then
-    echo "PALMmethod must be specified as 1 (PALM) or 2 (PALM-mbQTL)."
-    exit 1
-fi
-
-if [[ "${PALMmethod}" == 1 ]]; then
-    echo "Running PALM method..."
-else
-    echo "Running PALM-mbQTL method..."
-fi
-
 mkdir -p "${outputFolder}"
 
 # step0: check input files
@@ -27,11 +16,8 @@ echo "Finish: Check input files."
 
 # step1: fit null model for all phenotypes
 echo "Start: Fit null model."
-if [[ "${PALMmethod}" == 1 ]]; then
-    pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step1_palm.R \
-        --abdFile=${abdFile} \
-        --covFile=${covFile} \
-        --outputPrefix=${palm1_step1_prefix}
-else
-    # Remove PALM2
-fi
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step1_palm.R \
+    --abdFile=${abdFile} \
+    --covFile=${covFile} \
+    --outputPrefix=${palm1_step1_prefix}
+echo "Finish: Fit null model."
