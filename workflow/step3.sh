177a99dbc9a95ed28dc981b7bfaf489e8a07c82b
CheeseLee888 <cheese.lee.888@gmail.com>
2026-02-17 13:50:37 -0600

    Add step3.sh

---
+++ workflow/step3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+source "$WORK/config.sh"
+
+
+################################# workflow below (do not modify) #################################
+
+# step3: meta-analysis
+echo "Start: Performe meta analysis."
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step3_meta.R \
+    --files=${metaInfile} \
+    --outPrefix=${metaOutPrefix} \
+    --p.adjust.method=${metaPAdjustMethod} \
+    --meta.method=${metaMetaMethod}
+echo "Finish: Performe meta analysis."
