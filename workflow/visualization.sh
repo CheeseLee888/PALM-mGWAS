48c874ce95df999f387cd93eed05472f12a85342
CheeseLee888 <cheese.lee.888@gmail.com>
2026-02-21 17:27:05 -0600

    Rename step4 to step

--- workflow/visualization.sh
+++ workflow/visualization.sh
@@ -5,23 +5,23 @@ source "$WORK/config.sh"
 
 ################################# workflow below (do not modify) #################################
 echo "Start: Visualize results."
-pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step4_visualization.R \
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step_visualization.R \
     --metaDir=${metaDir} \
     --plotDir=${plotDir} \
     --pheno=${pheno} \
     --snp=${snp}
 
-pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step4_visualization.R \
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step_visualization.R \
     --metaDir=${metaDir} \
     --plotDir=${plotDir} \
     --pheno=${pheno}
 
-pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step4_visualization.R \
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step_visualization.R \
     --metaDir=${metaDir} \
     --plotDir=${plotDir} \
     --snp=${snp}
 
-pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step4_visualization.R \
+pixi run --manifest-path=${WORK}/pixi.toml Rscript ${WORK}/extdata/step_visualization.R \
     --metaDir=${metaDir} \
     --plotDir=${plotDir}
 echo "Finish: Visualize results."
